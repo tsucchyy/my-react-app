cc355246d88615bb8de27f6dd889a286cfde2a7b
Yasuyuki <tsuchy1625@icloud.com>
2020-06-06 17:52:01 +0900

    Updates

---
+++ precache-manifest.e2a471e57c2e8ce2e2d677f077125098.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b95fd037d7274ff1b4bbb6b8fc54807b",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "ef34aacd3f33bb18abd6",
+    "url": "/react/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "b953402f542e85d3a768",
+    "url": "/react/static/js/2.82419bec.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react/static/js/2.82419bec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef34aacd3f33bb18abd6",
+    "url": "/react/static/js/main.3b9d3a8e.chunk.js"
+  },
+  {
+    "revision": "adea5706a3232b20372a",
+    "url": "/react/static/js/runtime-main.6fa55cba.js"
+  }
+]);
